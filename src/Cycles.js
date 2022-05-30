cc355b8da6745837512697a760989567f605bf63
ДаниилГузанов <daniilg13@mail.ru>
2022-05-30 16:32:05 +0300

    Задание по работе с циклами

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,18 +3,36 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {}
+export function rangeSum(start, end) {
+    var c = 0;
+    for (; start <= end; start++) {
+        if (start % 2 == 0) c += start;
+    }
+    return c;
+}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {}
+export function iterationCount(a) {
+    var c = 0;
+    while (a > 0.1) {
+        c++;
+        a = a / 2.0;
+    }
+    return c;
+}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {}
+export function symbolsReplace(message) {
+    for (var i = 2; i < message.length; i += 3) {
+        message = message.substr(0, i) + '_' + message.substr(i + 1);
+    }
+    return message;
+}
